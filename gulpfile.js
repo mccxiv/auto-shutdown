05de418f7b03bce7e486134e3352e9b312ea850a
Andrea Stella <adr.stella@gmail.com>
2015-08-06 07:18:44 -0600

    Add build process

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,76 @@
 var exec = require('child_process').exec;
+var del = require('del');
 var gulp = require('gulp');
+var sequence = require('run-sequence');
+var packager = require('electron-packager');
+var vulcanize = require('gulp-vulcanize');
+var winInstaller = require('electron-windows-installer');
+
+var TEMP_BUILD_DIR = 'vulcanized';
+var BUILD_DIR = 'build';
+var DIST_DIR = 'dist';
+
+
+gulp.task('make-installer', function() {
+	return winInstaller({
+		appDirectory: BUILD_DIR + '/Auto-Shutdown-win32-ia32',
+		outputDirectory: DIST_DIR,
+		iconUrl: __dirname + 'src/assets/logo-gray.ico',
+		exe: 'Auto Shutdown.exe',
+		setupExe: 'Auto Shutdown Setup.exe',
+		title: 'Auto Shutdown',
+		setupIcon: __dirname + 'src/assets/logo-gray.ico'
+		//iconUrl: './src/assets/logo-gray.ico'
+	}); //.then(done).catch(done);
+});
 
 gulp.task('run-development', function(cb) {
 	exec('"src/node_modules/.bin/electron" ./src', cb);
 });
+
+gulp.task('run-vulcanized', function(cb) {
+	exec('"src/node_modules/.bin/electron" '+TEMP_BUILD_DIR, cb);
+});
+
+gulp.task('vulcanize', function() {
+	return gulp.src('src/index.html')
+		.pipe(vulcanize({
+			excludes: [],
+			inlineScripts: true,
+			inlineCss: true,
+			stripExcludes: false
+		}))
+		.pipe(gulp.dest(TEMP_BUILD_DIR));
+});
+
+gulp.task('copy-files', function() {
+	return gulp.src(['src/main.js', 'src/package.json'])
+		.pipe(gulp.dest(TEMP_BUILD_DIR));
+});
+
+gulp.task('clean-up-before', function(cb) {
+	del([TEMP_BUILD_DIR, BUILD_DIR, DIST_DIR], cb);
+});
+
+gulp.task('clean-up-after', function(cb) {
+	del([TEMP_BUILD_DIR, BUILD_DIR], cb);
+});
+
+gulp.task('package', function(cb) {
+	var opts = {
+		dir: 'vulcanized',
+		name: 'Auto-Shutdown',
+		platform: 'all',
+		arch: 'ia32',
+		version: '0.30.2',
+		icon: 'src/assets/logo-gray.ico',
+		out: BUILD_DIR
+	};
+	packager(opts, cb);
+});
+
+gulp.task('build', function(cb) {
+	sequence('clean-up-before', ['vulcanize', 'copy-files'], 'package', 'make-installer', 'clean-up-after', cb);
+});
+
+gulp.task('default', ['build']);
